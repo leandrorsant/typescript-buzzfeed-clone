a9359c578ca3d26db38b9bfaf7aaf723aa3a5e3f
Leandro <ramosdossantos.leandro@gmail.com>
2023-10-27 20:51:13 -0300

    server update

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const app = express()
 
 app.get("/quiz-item", async (req: Request, res: Response) => {
     try {
-        const response: AxiosResponse = await axios.get("https://b5eb6012-41b2-4b79-9166-8ec263c3c040-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/quizes/collections/quirky_quizes?page-size=3",{
+        const response: AxiosResponse = await axios.get(process.env.URL as string,{
             headers: {
                 'X-Cassandra-Token': process.env.TOKEN,
                 accept: 'application/json'
