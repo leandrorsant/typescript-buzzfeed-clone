888851223f614fe2a6d383dcf4b2ab8c15ad5d42
Leandro <ramosdossantos.leandro@gmail.com>
2023-10-28 06:27:02 -0300

    Implemented question title loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Title from './components/Title';
+import { Content, QuizData } from '../interfaces'
+import QuestionsBlock from './components/QuestionsBlock'
 
 function App() {
+  const [quiz, setQuiz] = useState<QuizData|null>(null)
+
+
+  const fetchData = async () => {
+    try{
+      const response = await fetch('http://localhost:8000/quiz-item')
+      const json = await response.json()
+      setQuiz(json)
+      
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  console.log(quiz)
+
   return (
     <div>
-      <Title/>
+      <Title title={quiz?.title} subtitle={quiz?.subtitle} />
+      {quiz?.content.map((content: Content, id: Content['id'])=>(
+        <QuestionsBlock
+          key={id} 
+          quizItem={content}
+        />
+      ))}
+      
     </div>
   );
 }
